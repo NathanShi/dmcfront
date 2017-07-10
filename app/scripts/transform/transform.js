3cdb17dadff5912bdcfc49a7863cb18e63d7b9e1
Clay Taylor <clay.taylor@e-gineering.com>
2017-07-10 12:47:18 -0400

    added css for homepage of curated path

---
+++ app/scripts/transform/transform.js
@@ -0,0 +1,26 @@
+'use strict';
+angular.module('dmc.transform', [
+    'ngMdIcons',
+    'ngAnimate',
+    'ngMaterial',
+    'ngSanitize',
+    'ui.router',
+    'md.data.table',
+    'dmc.configs.ngmaterial',
+    'dmc.common.header',
+    'dmc.common.footer',
+    'dmc.model.toast-model',
+    "dmc.ajax",
+    "dmc.data"
+]).config(function($stateProvider, $urlRouterProvider, $httpProvider){
+    $stateProvider.state('transform', {
+        template: '<ui-view />'
+    }).state('transform.home', {
+        url: '/',
+        controller: 'TransformController',
+        templateUrl: 'templates/transform/transform.html'
+    });
+    $urlRouterProvider.otherwise('/');
+}).controller('TransformController', function () {
+    
+});
