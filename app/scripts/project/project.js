bf3371a19d4f7fd88b41cd00e4b9047a96230c63
Yibing Shi <nathan@Yibings-MacBook-Pro.local>
2017-07-10 10:21:48 -0500

    Merge conflict fix

--- app/scripts/project/project.js
+++ app/scripts/project/project.js
@@ -41,7 +41,8 @@ angular.module('dmc.project', [
     'dmc.model.services',
     'dmc.widgets.project-tags',
     'dmc.sub-nav-menu',
-    'dmc.input-file.directive'
+    'dmc.input-file.directive',
+    'dmc.workspace-header'
 ])
     .config(function($stateProvider, $urlRouterProvider, $httpProvider){
 
