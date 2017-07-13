f7098fc2081ef09d1b4579525a2b82e82cfe11a5
Yibing Shi <nathan@Yibings-MacBook-Pro.local>
2017-07-13 14:53:17 -0500

    unused lines

--- app/scripts/company-onboarding/controller/home.js
+++ app/scripts/company-onboarding/controller/home.js
@@ -8,11 +8,6 @@ angular.module('dmc.company.onboarding')
 
 .controller('co-homeController', ['$scope', 'companyOnboardingModel', 'userData', 'DMCUserModel', '$rootScope', '$cookies', '$mdDialog',
     function($scope, companyOnboardingModel, userData, DMCUserModel, $rootScope, $cookies, $mdDialog){
-      $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-            'WY').split(' ').map(function (state) { return { abbrev: state }; });
-
-      $scope.tierLevel = 'Tier3';
       $cookies.put('fromDMDIISignup',true)
 
 }])
