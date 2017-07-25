a3d8a5da57eb036676210467974dfb7ad67afdb2
Yibing Shi <nathan@Yibings-MacBook-Pro.local>
2017-07-25 09:39:07 -0500

    remove * for required input

--- app/scripts/company-onboarding/controller/companyinfo.js
+++ app/scripts/company-onboarding/controller/companyinfo.js
@@ -32,9 +32,19 @@ angular.module('dmc.company.onboarding')
               }
 
               else{
-                alert("You already have a Tier3 Membership organization database, will redirect to dashboard");
-                $window.location.href = '/onboarding.php';
-                // $('#orgExist').modal('show').
+                // alert("You already have a Tier3 Membership organization database, will redirect to dashboard");
+                // $window.location.href = '/onboarding.php';
+                $mdDialog.show(
+                  $mdDialog.alert()
+                    // .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('This is an alert title')
+                    .textContent('You can specify some description text in here.')
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('Got it!')
+                    .targetEvent(ev)
+                );
+                // $('#orgExist').modal('show');
                 // $('#orgExist').on('hidden.bs.modal', function (e) {
                 //   $window.location.href = '/onboarding.php';
                 // });
