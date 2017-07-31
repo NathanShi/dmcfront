ed34b48e23c388af3acb20af0c80b9f1b9d4c49a
Clay Taylor <clay.taylor@e-gineering.com>
2017-07-31 15:46:27 -0400

    added comments to the directive for ease of use

--- app/scripts/components/ui-widgets/content-card.directive.js
+++ app/scripts/components/ui-widgets/content-card.directive.js
@@ -23,10 +23,37 @@ angular.module('dmc.widgets.content', [
         controllerAs: '$ctrl'
     };
     
+    /**
+     * Controller for the uiWidgetContent directive.
+     *
+     * This directive contains 4 properties or objects:
+     * contentType: String (required) - 'app', 'video', 'img', or 'document'
+     * title: String (optional) - any string to be displayed
+     * category: String (optional) - any string to be displayed
+     * contentItems: Array of objects
+     *
+     * This directive can display 4 categories of content for the contentItems array:
+     *
+     * document - array of documents
+     * video - [{
+     *          videoSource: String (link to video)
+     *          contentTitle: String (video title)
+     *          description: String (description of video)
+     *          buttonText: String (text to be displayed on button)
+     *          buttonLink: String (link to new page)
+     *         }]
+     * img - [{
+     *         imgSource: String (link to img)
+     *         contentTitle: String (video title)
+     *         description: String (description of video)
+     *         buttonText: String (text to be displayed on button)
+     *         buttonLink: String (link to new page)
+     *       }]
+     * app - array of apps/services
+     *
+     */
     function UiWidgetContentCardController($http) {
         var vm = this;
-        
-        console.log(vm.contentItems);
         
         vm.downloadFile = function(id) {
             window.location = dataFactory.documentsUrl(id).download;
