647b9eceb5c2b6addd6d94a806daf32ddc60ddfb
Yibing Shi <nathan@Yibings-MacBook-Pro.local>
2017-08-24 10:25:49 -0500

    comment console log

--- app/scripts/company-onboarding/controller/companyinfo.js
+++ app/scripts/company-onboarding/controller/companyinfo.js
@@ -156,10 +156,10 @@ angular.module('dmc.company.onboarding')
         if ($scope.company.id){
           $scope.companyinfo.id = $scope.company.id;
         }
-        console.log($scope.companyinfo);
+        // console.log($scope.companyinfo);
         //Call service.populateField() to stream info for Membership Agreement
         var postDocInfo = companyOnboardingModel.populateField($scope.companyinfo);
-        console.log(postDocInfo);
+        // console.log(postDocInfo);
         var responseErrorReason = "Oops, we had a problem when generating Membership Agreement, please try again later. " +
         "\nIf you kept having this problem, please contact us.";
 
